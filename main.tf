c26f15bd56d99a34614d25e1b8cdfbdcd156a0c8
mohan <mohanprasad390@gmail.com>
2025-11-26 17:24:18 +0530

    message

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ resource "aws_security_group" "main" {
   description = var.description
   vpc_id      = var.vpc_id
 
-  tags = merge {
+  tags = merge(
     var.sg_tags,
     local.common_tags,
     {
         Name = "${var.project}-${var.environment}-${var.sg_name}"
     }
-  }
+  )
 }
