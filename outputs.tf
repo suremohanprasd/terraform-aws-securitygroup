deb091f23b8e9e3383da1cbf001bf59a5fed03a6
mohan <mohanprasad390@gmail.com>
2025-12-17 14:52:01 +0530

    message

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 output "sg_id" {
     value = aws_security_group.main.id
 }
+
+output "public_subnet_ids" {
+    value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+    value = aws_subnet.database[*].id
+}
